057f810a3f8caef55a35ef9c5904e8e1c3735fb6
saiprince2595 <sai.p23@gmail.com>
2017-09-24 09:22:46 +0530

    Node Modules added

---
+++ src/app/hero-detail-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'hero-detail',
+    template: `
+    <div>This is component</div>
+  `
+})
+export class HeroDetailComponent {
+
+}
